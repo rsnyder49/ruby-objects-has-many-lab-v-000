c72fdaa3e292680e07e8fff9ee4cb924a469e601
Robert Snyder <r.snyder49@gmail.com>
2018-08-28 22:27:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,15 +8,15 @@ class Artist
   end 
   
   def add_song(song)
-    @songs << song
     song.artist = self
+    @songs << song
     @@all_songs << song
   end 
   
   def add_song_by_name(song_name)
     song = Song.new(song_name)
-    @songs << song
     song.artist = self
+    @songs << song
     @@all_songs << song 
   end 
   
